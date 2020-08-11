53ff71425a915115dc25717b3e217014335adce9
nickariley <nickariley@gmail.com>
2020-08-11 05:05:51 -0500

    Task Item works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DateTimeComponent } from './date-time/date-time.component';
 import { MessagePreviewComponent } from './message-preview/message-preview.component';
+import { TaskItemComponent } from './task-item/task-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DateTimeComponent,
-    MessagePreviewComponent
+    MessagePreviewComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule
