f3bbe46f788870307df228742c53e4cd76efa66d
nickariley <nickariley@gmail.com>
2020-08-11 16:26:34 -0500

    Transactions Panel working

--- src/app/transactions-panel/transactions-panel.component.ts
+++ src/app/transactions-panel/transactions-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import orders from '../orders';
 
 @Component({
   selector: 'app-transactions-panel',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionsPanelComponent implements OnInit {
 
+  orders: any[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.orders = orders;
   }
 
 }
